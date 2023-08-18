a5acddf31df2a84b4fee2ee4669a8e7dde803bd8
Anilesh Prajapati <65806802+anilesh-prajapati@users.noreply.github.com>
2023-08-18 22:29:47 +0530

    Add files via upload

---
+++ PDFChatBotApp/app.py
@@ -0,0 +1,129 @@
+
+
+
+
+import streamlit as st
+from dotenv import load_dotenv
+import pickle
+from PyPDF2 import PdfReader
+from streamlit_extras.add_vertical_space import add_vertical_space
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
+import os
+
+
+
+
+load_dotenv()
+
+def main():
+    
+    
+    # Sidebar contents
+    with st.sidebar:
+        st.title('🤗💬 Chat with PDF App💬')
+        add_vertical_space(1)
+        # upload a PDF file
+        pdf = st.file_uploader("Upload your PDF", type='pdf')
+        add_vertical_space(2)
+        st.markdown('''
+        ## About
+        This app is an LLM-powered chatbot built using:
+        - [Streamlit](https://streamlit.io/)
+        - [LangChain](https://python.langchain.com/)
+        - [OpenAI](https://platform.openai.com/docs/models) LLM model
+    
+        ''')
+        
+        st.write('Made with ❤️ by [Anilesh Prajapati](www.linkedin.com/in/anilesh-prajapati-5b5748239)')
+
+
+    
+
+    # st.write(pdf)
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+            )
+        chunks = text_splitter.split_text(text=text)
+
+        # # embeddings
+        store_name = pdf.name[:-4]
+        
+        # st.write(f'{store_name}')
+        # st.write(chunks)
+
+        if os.path.exists(f"{store_name}.pkl"):
+            with open(f"{store_name}.pkl", "rb") as f:
+                VectorStore = pickle.load(f)
+            # st.write('Embeddings Loaded from the Disk')s
+        else:
+            embeddings = OpenAIEmbeddings()
+            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+            with open(f"{store_name}.pkl", "wb") as f:
+                pickle.dump(VectorStore, f)
+
+        # embeddings = OpenAIEmbeddings()
+        # VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+        
+        # Initialize chat history
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+            
+        # Display chat messages from history on app rerun
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+                
+                
+
+        # Accept user questions/query
+        
+        # React to user input
+        if query := st.chat_input("What is up?"):
+            # Display user message in chat message container
+            st.chat_message("user").markdown(query)
+            # Add user message to chat history
+            st.session_state.messages.append({"role": "user", "content": query})
+            
+            
+            
+            docs = VectorStore.similarity_search(query=query, k=3)
+
+            llm = OpenAI()
+            chain = load_qa_chain(llm=llm, chain_type="stuff")
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents=docs, question=query)
+                print(cb)
+            
+            # Display assistant response in chat message container
+            with st.chat_message("assistant"):
+                st.markdown(response)
+            # Add assistant response to chat history
+            st.session_state.messages.append({"role": "assistant", "content": response})
+      
+        
+
+        
+            
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
